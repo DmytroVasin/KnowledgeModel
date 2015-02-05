cb932fe0c62f7e5b60366ca26804e0fc378b1855
Vasin Dmitriy <vasindima779@gmail.com>
2015-02-05 21:30:02 +0200

    Properly worked version

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,37 @@
 class MainViewController < UIViewController
   def loadView
     self.view = MainView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    self.view.addSubview( start_button )
   end
 
-  def start_test
+  def start_button
+    UIButton.buttonWithType(UIButtonTypeCustom).tap do |button|
+      button.frame = [
+        [self.view.frame.size.width/2 - 100, self.view.frame.size.height - 55],
+        [200, 40]
+      ]
+
+      button.setTitle('Начать', forState: UIControlStateNormal)
+      button.styleId = 'start_button'
+
+      button.addTarget(self, action: 'start_testing', forControlEvents: UIControlEventTouchUpInside)
+    end
+  end
+
+
+  def viewDidLoad
+  end
+
+  def start_testing
+    puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
     questionViewController = QuestionViewController.alloc.init
     questionViewController.delegate = self
 
     self.presentViewController questionViewController, animated:true, completion:nil
+  end
+
+  def done_with_b
+    self.view = MainView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    self.view.addSubview( start_button )
   end
 end
--- app/views/main_view.rb
+++ app/views/main_view.rb
@@ -9,21 +9,6 @@ class MainView < UIView
           [self.frame.size.width - 40 , self.frame.size.height - 95]
         ]
       ))
-      addSubview( start_button )
-    end
-  end
-
-  def start_button
-    UIButton.buttonWithType(UIButtonTypeCustom).tap do |button|
-      button.frame = [
-        [self.frame.size.width/2 - 100, self.frame.size.height - 55],
-        [200, 40]
-      ]
-
-      button.setTitle('Начать', forState: UIControlStateNormal)
-      button.styleId = 'start_button'
-
-      button.addTarget(self, action: 'start_test', forControlEvents: UIControlEventTouchUpInside)
     end
   end
 end
