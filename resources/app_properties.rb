30a6db7b36b42cef887f35295b07138405a3a960
Vasin Dmitriy <vasindima779@gmail.com>
2015-05-13 15:01:02 +0300

    Changed icons

--- resources/app_properties.rb
+++ resources/app_properties.rb
@@ -1,6 +1,6 @@
 class AppProperties
   ID    = 'com.rails-junior.app'
-  NAME    = 'Ruby/Rails Test'
+  NAME    = 'Ruby Rails Test'
   VERSION = '1.0.0'
 
   def name
@@ -17,8 +17,20 @@ class AppProperties
 
   def icons
     [
-    'icons/j-way-icon-iphone.png',
-    'icons/j-way-icon-iphone-retina.png'
+      'icons/j-way-icon-iphone-40-small.png',
+      'icons/j-way-icon-iphone-40-small@2x.png',
+      'icons/j-way-icon-iphone-40-small@3x.png',
+      'icons/j-way-icon-iphone-60@2x.png',
+      'icons/j-way-icon-iphone-60@3x.png',
+      'icons/j-way-icon-iphone-72.png',
+      'icons/j-way-icon-iphone-72@2x.png',
+      'icons/j-way-icon-iphone-76@.png',
+      'icons/j-way-icon-iphone-76@2x.png',
+      'icons/j-way-icon-iphone-small.png',
+      'icons/j-way-icon-iphone-small@2x.png',
+      'icons/j-way-icon-iphone-small@3x.png',
+      'icons/j-way-icon-iphone.png',
+      'icons/j-way-icon-iphone@2x.png',
     ]
   end
 
@@ -44,18 +56,14 @@ class AppProperties
 
 
   def dev_certificate
-    ENV['DEVELOPMENT_CERTIFICATE_NAME']
   end
 
   def dev_profile_path
-    ENV['DEVELOPMENT_PROVISIONING_PROFILE_PATH']
   end
 
   def release_certificate
-    ENV['RELEASE_CERTIFICATE_NAME']
   end
 
   def release_profile_path
-    ENV['RELEASE_PROVISIONING_PROFILE_PATH']
   end
 end
